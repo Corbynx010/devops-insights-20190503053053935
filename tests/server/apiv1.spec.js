9e305f8ce656e8fb085835b8d25be3e53f10fa6a
Corbyn Noble-May <corbynx010@hotmail.co.nz>
2019-05-05 05:09:49 +0000

    unit tests for city query

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -233,6 +233,56 @@
       assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
       assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
       assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 25.56 °C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
-    });    
+    }); 
+
+    it('without city', function() {
+      reqMock = {
+        query: {
+
+        }
+      };
+
+      apiv1.getWeather3(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
+    });
+
+    it('with valid city and error from request call', function() {
+      reqMock = {
+        query: {
+          city: 'Hamilton'
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback("error", null, null);
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeather3(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
+    });
+
+    it('with incomplete city', function() {
+      reqMock = {
+        query: {
+          city: 'Hamiloyt'
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback(null, null, {});
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeather3(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
+    });  
   });
 }());
